48b6ee4f6b26d6cc57ad8870e2eae73c2baea1ee
beha16 <behzod0824@gmail.com>
2024-08-29 17:51:14 +0500

    ok

--- root/settings.py
+++ root/settings.py
@@ -123,8 +123,7 @@ STATIC_FILE = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
