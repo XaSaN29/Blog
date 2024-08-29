990763806cbfa2f1499c0aea31791be8ddf7408b
xasan <fatxuddinovx@gmail.com>
2024-08-29 17:47:09 +0500

    login zor

--- apps/models.py
+++ apps/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
+class User(AbstractUser):
+    image = models.ImageField(upload_to='user/', blank=True, null=True)
+    about_me = models.TextField(blank=True, null=True)
 
 
 
--- root/settings.py
+++ root/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'root.urls'
+AUTH_USER_MODEL  = 'apps.User'
 
 TEMPLATES = [
     {
@@ -117,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
