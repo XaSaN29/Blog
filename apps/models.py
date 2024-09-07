587b2fe4e8b731a1ccdd1c531f6039df554317a3
beha16 <behzod0824@gmail.com>
2024-09-07 17:49:10 +0500

    post page was created

--- apps/models.py
+++ apps/models.py
@@ -3,9 +3,22 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
 class User(AbstractUser):
     image = models.ImageField(upload_to='user/', blank=True, null=True)
     about_me = models.TextField(blank=True, null=True)
+
+
+class Post(BaseModel):
+    image = models.ImageField(upload_to='post/', blank=True, null=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
 
 
